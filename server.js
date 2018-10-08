6bc0253d85440004aa34f6db6a493af9e912897d
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-08 16:17:07 -0700

    Adds mongodb and mongoose setup

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const db = require('./keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log("Connected to mongoDB."))
+    .catch(err => console.log(err));
+
 const port = 8989;
 app.listen(port, () => console.log(`Server running on port:${port}`));
