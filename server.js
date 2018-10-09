13e7c5636d08056ab642313d224ee2fedc8673b0
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-08 18:45:28 -0700

    Adds body-parser to server.js

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./keys').mongoURI;
 
+// Body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // Routes
 const users = require('./routes/api/users');
 
-// App
 app.use('/api/users', users);
 
 
