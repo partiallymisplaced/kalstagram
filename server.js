3dac9bc866424c5113d2111d145ae7c4e76aeeb6
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-08 17:05:57 -0700

    Adds users route to server

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+// Express
 const express = require('express');
 const app = express();
 
+// Mongoose
 const mongoose = require('mongoose');
 const db = require('./keys').mongoURI;
+
+// Routes
+const users = require('./routes/api/users');
+
+// App
+app.use('/api/users', users);
+
 
 mongoose
     .connect(db)
     .then(() => console.log("Connected to mongoDB."))
     .catch(err => console.log(err));
 
+// Port
 const port = 8989;
 app.listen(port, () => console.log(`Server running on port:${port}`));
