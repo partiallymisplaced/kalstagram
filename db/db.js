59ddcae1f6f7f369b97c900a3fb5d9eff207dc41
Nevena Stoeva <1844100+partiallymisplaced@users.noreply.github.com>
2018-10-09 22:25:46 -0700

    Revert "profile model and route"

--- db/db.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-const db = 'mongodb://localhost:27017/kalstagram-db';
-module.exports = db;
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/', function(req, res, next){
-    // res.send('hello from index');
-    res.render('index', { title: 'Profile', message: 'Welcome to Kalstagram'});
-});
-module.exports = router;
