0a81d08d9b5a1e107874413365706e038c96f11e
Nevena Stoeva <1844100+partiallymisplaced@users.noreply.github.com>
2018-10-09 22:29:28 -0700

    Revert "profile model and route" (#2)
    Not ready to be merged.

--- routes/api/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Profile = require('../../profile-model/Profile');
-router.get('/', function(req, res, next){
-    // res.send('hello from profile');
-    res.render('profile', { title: 'Profile', fullname: 'Hiwot Nega'});
-});
-module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const index = require('./routes/api/index');
-const profile = require('./routes/api/profile');
-const db = require('./db/db');
-const createProfile = require('./queries/createProfile');
 const app = express();
-//connect to db
-    mongoose
-    .connect(db,{useNewUrlParser: true})
-    .then(() => {
-        console.log('Connected to mongoDB.....!!!');
-    }).catch(() =>console.log('unable to connect to mongoDB....!!!'));
-//createProfile
-createProfile();
-//bodyparser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-//settings
-app.set('views', './views');
-app.set('view engine', 'pug');
-//routing 
-app.use('/', index);
-app.use('/profile', profile);
+
+const mongoose = require('mongoose');
+const db = require('./keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log("Connected to mongoDB."))
+    .catch(err => console.log(err));
 
 const port = 8989;
 app.listen(port, () => console.log(`Server running on port:${port}`));
